d37acfb397ee321b1123eb4485edea8d2df3a62a
BangEunchan <bang9911@gmail.com>
2022-09-14 23:06:46 +0900

    saveFile finish

--- WordMasterProject/src/com/mycom/word/WordCRUD.java
+++ WordMasterProject/src/com/mycom/word/WordCRUD.java
@@ -3,7 +3,9 @@ package com.mycom.word;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -135,7 +137,16 @@ public class WordCRUD implements ICRUD{
 	}
 
 	public void saveFile() {
-		// TODO Auto-generated method stub
-		
+		try {
+			PrintWriter pr = new PrintWriter(new FileWriter(fname));
+			for(Word one : list) {
+				pr.write(one.toFileString() + "\n");
+			}
+			pr.close();
+			System.out.println("==> 데이터 저장 완료 !!!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
