156c9382c8b7472cfc3d61365859bc945514a4d2
BangEunchan <bang9911@gmail.com>
2022-09-14 22:05:14 +0900

    Add "ArrayList<Integer> listAll" and update

--- WordMasterProject/src/com/mycom/word/WordCRUD.java
+++ WordMasterProject/src/com/mycom/word/WordCRUD.java
@@ -56,6 +56,23 @@ public class WordCRUD implements ICRUD{
 		}
 		System.out.println("------------------------------");
 	}
+	
+	public ArrayList<Integer> listAll(String keyword) {
+		
+		ArrayList<Integer> idlist = new ArrayList<>();
+		int j = 0;
+		System.out.println("------------------------------");
+		for(int i = 0; i < list.size(); i++) {
+			String word = list.get(i).getWord();
+			if(word.contains(keyword)) continue;
+			System.out.print((j+1) + " ");
+			System.out.println(list.get(i).toString());
+			idlist.add(i);
+			j++;
+		}
+		System.out.println("------------------------------");
+		return idlist;
+	}
 
 	public void updateItem() {
 		System.out.print("=> 수정할 단어 검색 : ");
